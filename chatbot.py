8ea06a8a3ee5a885e6ba821c417e4a1ce3d01348
edsontjrmkt <edsontjrmktdigital@gmail.com>
2025-08-14 00:12:59 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -89,12 +89,12 @@ class Config:
     AUDIO_DURATION = 7
     IMG_PROFILE = "https://i.ibb.co/ks5CNrDn/IMG-9256.jpg"
     IMG_GALLERY = [
-        "https://i.ibb.co/zhNZL4FF/IMG-9198.jpg",
+        "https://i.ibb.co/C5dGymT6/calcinha.png",
         "https://i.ibb.co/Y4B7CbXf/IMG-9202.jpg",
         "https://i.ibb.co/Fqf0gPPq/IMG-9199.jpg"
     ]
     IMG_HOME_PREVIEWS = [
-        "https://i.ibb.co/k2MJg4XC/Save-ClipApp-412457343-378531441368078-7870326395110089440-n.jpg",
+        "https://i.ibb.co/C5dGymT6/calcinha.png",
         "https://i.ibb.co/MxqKBk1X/Save-ClipApp-481825770-18486618637042608-2702272791254832108-n.jpg",
         "https://i.ibb.co/F4CkkYTL/Save-ClipApp-461241348-1219420546053727-2357827070610318448-n.jpg"
     ]
@@ -1681,5 +1681,6 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
